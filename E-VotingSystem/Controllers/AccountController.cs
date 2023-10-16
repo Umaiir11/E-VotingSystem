d5e7e108f2d9ffafe1b1b78f3bbf928f373c73ac
Umair11 <iam.umairimran@gmail.com>
2023-10-16 13:11:38 +0500

    Add project files.

---
+++ E-VotingSystem/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using E_VotingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace E_VotingSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection l_SqlConnection = new SqlConnection();
+        SqlCommand l_SqlCommand = new SqlCommand();
+        SqlDataReader? l_SqlDataReader;
+
+        // GET: Account
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        void FncConnectionString() {
+
+            l_SqlConnection.ConnectionString = "Data Source=MUHAMMAD-UMAIR\\AISONESQL;Initial Catalog=EVoting;Integrated Security=True";
+        }
+        [HttpPost]
+        public ActionResult Verify( ModUser lModUser ) { 
+
+            FncConnectionString();  
+            l_SqlConnection.Open(); 
+            l_SqlCommand.Connection= l_SqlConnection;
+			l_SqlCommand.CommandText = "select * from TB_USER  where username='" + lModUser.PrUserName + "' and password='" + lModUser.PrPassword + "'";
+			l_SqlDataReader = l_SqlCommand.ExecuteReader();
+
+            if (l_SqlDataReader.Read())
+            {
+
+                l_SqlConnection.Close();
+                return View("Create");
+               
+
+            }
+
+            else {
+
+                l_SqlConnection.Close();
+                return View("Error");
+            }
+               
+        }
+    }
+}
---
+++ E-VotingSystem/Models/ModUser.cs
@@ -0,0 +1,8 @@
+﻿namespace E_VotingSystem.Models
+{
+    public class ModUser
+    {
+        public string? PrUserName { get; set; }
+        public string? PrPassword { get; set;}
+    }
+}
