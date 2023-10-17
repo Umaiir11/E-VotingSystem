cf3fcd799e09e5858940729d670a005a2fa30224
Umair11 <iam.umairimran@gmail.com>
2023-10-17 15:32:08 +0500

    new screens added user profile , error, errorpassword

--- E-VotingSystem/Controllers/AccountController.cs
+++ E-VotingSystem/Controllers/AccountController.cs
@@ -17,34 +17,60 @@ namespace E_VotingSystem.Controllers
             return View();
         }
 
-        void FncConnectionString() {
-
+        void FncConnectionString()
+        {
             l_SqlConnection.ConnectionString = "Data Source=MUHAMMAD-UMAIR\\AISONESQL;Initial Catalog=EVoting;Integrated Security=True";
         }
-        [HttpPost]
-        public ActionResult Verify( ModUser lModUser ) { 
 
-            FncConnectionString();  
-            l_SqlConnection.Open(); 
-            l_SqlCommand.Connection= l_SqlConnection;
-			l_SqlCommand.CommandText = "select * from TB_USER  where username='" + lModUser.PrUserName + "' and password='" + lModUser.PrPassword + "'";
-			l_SqlDataReader = l_SqlCommand.ExecuteReader();
+        [HttpPost]
+        public ActionResult Verify(ModUser lModUser)
+        {
+            FncConnectionString();
+            l_SqlConnection.Open();
+            l_SqlCommand.Connection = l_SqlConnection;
+            l_SqlCommand.CommandText = "SELECT * FROM TBU_USER WHERE MembershipID = @MembershipID AND Password = @Password";
+            l_SqlCommand.Parameters.AddWithValue("@MembershipID", lModUser.MembershipID);
+            l_SqlCommand.Parameters.AddWithValue("@Password", lModUser.Password);
+            l_SqlDataReader = l_SqlCommand.ExecuteReader();
+            bool l_MobileFieldEmpty = false;
 
             if (l_SqlDataReader.Read())
             {
+                ModUser l_ModloggedInUser = new ModUser
+                {
+                    PKGUID = l_SqlDataReader["PKGUID"] as string,
+                    MembershipID = l_SqlDataReader["MembershipID"] as string,
+                    MemberName = l_SqlDataReader["MemberName"] as string,
+                    Address1 = l_SqlDataReader["Address1"] as string,
+                    Address2 = l_SqlDataReader["Address2"] as string,
+                    Email = l_SqlDataReader["Email"] as string,
+                    FounderMembers = l_SqlDataReader["FounderMembers"] as string,
+                    LifeAndAnnualMembers = l_SqlDataReader["LifeAndAnnualMembers"] as string,
+                    CompanysName = l_SqlDataReader["CompanysName"] as string,
+                    Region = l_SqlDataReader["Region"] as string,
+                    Password = lModUser.Password, // Set the password from the user input
+                    Mobile = l_SqlDataReader["Mobile"] as int?,
+                    ContactNo = l_SqlDataReader["ContactNo"] as int?
+                };
 
+                if (!l_ModloggedInUser.Mobile.HasValue)
+                {
+                    l_MobileFieldEmpty = true;
+                }
                 l_SqlConnection.Close();
-                return View("Create");
-               
 
+                if (l_MobileFieldEmpty)
+                {
+                    return View("Error");
+                }
+
+                return RedirectToAction("Index", "Profile", l_ModloggedInUser);
             }
-
-            else {
-
+            else
+            {
                 l_SqlConnection.Close();
-                return View("Error");
+                return View("ErrorPassword"); // Password is incorrect
             }
-               
         }
     }
 }
---
+++ E-VotingSystem/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using E_VotingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_VotingSystem.Controllers
+{
+	public class ProfileController : Controller
+	{
+		public IActionResult Index(ModUser l_ModLoggedInUser )
+		{
+			return View(l_ModLoggedInUser);
+		}
+	}
+}
